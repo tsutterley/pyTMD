bb4fa4803f2359c15ef542db734cf33bd600b48d
tsutterley <tsutterl@uw.edu>
2022-12-14 15:27:36 -0800

    imports for deprecated modules to not break current workflows

--- pyTMD/__init__.py
+++ pyTMD/__init__.py
@@ -30,5 +30,21 @@ from pyTMD.model import model
 from pyTMD.nearest_extrap import nearest_extrap
 from pyTMD.tidal_ellipse import tidal_ellipse
 
+# deprecated modules
+from pyTMD.calc_delta_time import calc_delta_time
+from pyTMD.compute_equilibrium_tide import compute_equilibrium_tide
+from pyTMD.iers_mean_pole import iers_mean_pole
+from pyTMD.infer_minor_corrections import infer_minor_corrections
+from pyTMD.output_otis_tides import output_otis_grid, output_otis_elevation, output_otis_transport
+from pyTMD.read_tide_model import extract_tidal_constants
+from pyTMD.read_netcdf_model import extract_netcdf_constants
+from pyTMD.read_GOT_model import extract_GOT_constants
+from pyTMD.read_FES_model import extract_FES_constants
+from pyTMD.predict_tidal_ts import predict_tidal_ts
+from pyTMD.predict_tide_drift import predict_tide_drift
+from pyTMD.predict_tide import predict_tide
+from pyTMD.read_iers_EOP import read_iers_EOP
+from pyTMD.read_ocean_pole_tide import read_ocean_pole_tide
+
 # get semantic version from setuptools-scm
 __version__ = pyTMD.version.version
