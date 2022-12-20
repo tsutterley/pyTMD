1771299a4ccdb73be59eb7a95bb523ee365fddec
tsutterley <tsutterl@uw.edu>
2022-12-19 17:05:39 -0800

    test: read header from OPT test file and compare more variables

--- test/test_ocean_pole_tide.py
+++ test/test_ocean_pole_tide.py
@@ -6,6 +6,7 @@ Written by Tyler Sutterley (12/2022)
 UPDATE HISTORY:
     Updated 12/2022: single implicit import of pyTMD
         use constants class for ellipsoidal parameters
+        read header from test file and compare more variables
     Updated 07/2022: define variable formats of test inputs
     Updated 05/2022: change to longcomplex for windows compatibility
     Written 08/2020
@@ -27,12 +28,32 @@ filepath = os.path.dirname(os.path.abspath(filename))
 @pytest.mark.parametrize("METHOD", ['spline','nearest','linear'])
 # test the interpolation of ocean pole tide values
 def test_ocean_pole_tide(METHOD):
+    # read ocean pole tide test file for header text
+    ocean_pole_test_file = os.path.join(filepath,'opoleloadcmcor.test')
+    with open(ocean_pole_test_file, mode='r', encoding='utf8') as f:
+        file_contents = f.read().splitlines()
+
+    # extract header parameters
+    rx = re.compile(r'(.*?)\s+\=\s+([-+]?\d+\.\d+[e][-+]?\d+)', re.VERBOSE)
+    header_contents = [l for l in file_contents if rx.match(l)]
+    test_header = {}
+    for line in header_contents:
+        param,contents = rx.findall(line).pop()
+        test_header[param] = np.float64(contents)
+
+    # extract longitude and latitude from header
+    lat = re.findall(r'latitude = ([-+]?\d+\.\d+) degrees', file_contents[1]).pop()
+    lon = re.findall(r'longitude = ([-+]?\d+\.\d+) degrees', file_contents[1]).pop()
+    test_header['latitude'] = np.float64(lat)
+    test_header['longitude'] = np.float64(lon)
+
     # degrees to radians and arcseconds to radians
     dtr = np.pi/180.0
     atr = np.pi/648000.0
     # earth and physical parameters for ellipsoid
     units = pyTMD.constants('IERS')
-    G = 6.673e-11# test universal constant of gravitation [m^3/(kg*s^2)]
+    # universal constant of gravitation for test [m^3/(kg*s^2)]
+    G = 6.673e-11
     # mean equatorial gravitational acceleration [m/s^2]
     ge = 9.7803278
     # density of sea water [kg/m^3]
@@ -45,29 +66,27 @@ def test_ocean_pole_tide(METHOD):
     K = 4.0*np.pi*G*rho_w*Hp*units.a_axis/(3.0*ge)
     K1 = 4.0*np.pi*G*rho_w*Hp*units.a_axis**3/(3.0*units.GM)
 
-    # determine differences with values from test data
+    # determine differences with constants from test data
     eps = np.finfo(np.float16).eps
-    a_validation = 6.3781366000e+06
-    Hp_validation = 2.8577142980e+04
-    K_validation = 5.3394043696e+03
-    assert (np.abs(units.a_axis - a_validation) < eps)
-    assert (np.abs(Hp - Hp_validation) < eps)
-    assert (np.abs(K - K_validation) < eps)
+    assert np.isclose(units.a_axis, test_header['aE'])
+    assert np.isclose(units.omega, test_header['Omega'])
+    assert np.isclose(units.GM, test_header['GM'])
+    assert np.isclose(rho_w, test_header['rhow'])
+    assert np.isclose(gamma.real, test_header['gamma2(real)'])
+    assert np.isclose(gamma.imag, test_header['gamma2(imag)'])
+    assert np.isclose(G, test_header['G'])
+    assert np.isclose(Hp, test_header['Hp'])
+    assert np.isclose(K, test_header['K'])
 
     # read test file for values
-    ocean_pole_test_file = os.path.join(filepath,'opoleloadcmcor.test')
     names = ('MJD','xbar_p','ybar_p','x_p','y_p','m1','m2','u_radial','u_north','u_east')
     formats = ('i4','f4','f4','f4','f4','f4','f4','f4','f4','f4')
     validation = np.loadtxt(ocean_pole_test_file, skiprows=26,
         dtype=dict(names=names, formats=formats))
     file_lines = len(validation)
     # mean pole coordinates for test
-    xmean = np.array([5.4e-2, 8.30e-4])
-    ymean = np.array([3.57e-1, 3.95e-3])
-    # test date in Modified Julian Days
-    t0 = 5.1544e4
-    # coordinates for test
-    lon,lat = (232.25,-43.75)
+    xmean = np.array([test_header['xmean(t0)'], test_header['xmeandot']])
+    ymean = np.array([test_header['ymean(t0)'], test_header['ymeandot']])
 
     # read ocean pole tide map from Desai (2002)
     ocean_pole_tide_file = pyTMD.utilities.get_data_path(
@@ -82,17 +101,19 @@ def test_ocean_pole_tide(METHOD):
         f2 = scipy.interpolate.RectBivariateSpline(ilon, ilat[::-1],
             iur[:,::-1].imag, kx=1, ky=1)
         UR = np.zeros((file_lines),dtype=np.longcomplex)
-        UR.real = f1.ev(lon,lat)
-        UR.imag = f2.ev(lon,lat)
+        UR.real = f1.ev(test_header['longitude'], test_header['latitude'])
+        UR.imag = f2.ev(test_header['longitude'], test_header['latitude'])
     else:
         # use scipy regular grid to interpolate values for a given method
         r1 = scipy.interpolate.RegularGridInterpolator((ilon, ilat[::-1]),
             iur[:,::-1], method=METHOD)
-        UR = r1.__call__(np.c_[lon,lat])
+        UR = r1.__call__(np.c_[test_header['longitude'], test_header['latitude']])
 
+    # calculate differences in time
+    dt = (validation['MJD'] - test_header['t0'])/test_header['1 year']
     # calculate angular coordinates of mean pole at time
-    mpx = xmean[0] + xmean[1]*(validation['MJD'] - t0)/365.25
-    mpy = ymean[0] + ymean[1]*(validation['MJD'] - t0)/365.25
+    mpx = xmean[0] + xmean[1]*dt
+    mpy = ymean[0] + ymean[1]*dt
 
     # calculate differentials from mean pole positions
     mx = validation['x_p'] - mpx
